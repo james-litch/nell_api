9f6704444430b44a3835c39c1762a3b85b3b694e
James Litchfield <james.litchfield99@gmail.com>
2019-12-25 19:42:17 +0000

    added the user schema

---
+++ src/typeDefs/user.js
@@ -0,0 +1,25 @@
+import { gql } from 'apollo-server-express'
+
+export default gql`
+    extend type Query{
+        user(id: ID!): User
+        users: [User!]!
+        me: User
+    }
+
+    extend type Mutation{
+        signUp(email: String!, name: String!, password: String!): AuthData
+        signIn(email: String!, password: String!): AuthData
+    }
+
+    type User{
+        id: ID!
+        email: String!
+        name: String!
+        createdAt: String!
+    }
+
+    type AuthData{
+        token: String!
+    }
+`
