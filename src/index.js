77f231dba2e753e32d3e777f4e22d33f4b7e23e4
James Litchfield <james.litchfield99@gmail.com>
2019-12-25 19:46:02 +0000

    added basic server functionality

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import mongoose from 'mongoose'
+import typeDefs from './typeDefs'
+import resolvers from './resolvers'
+import {
+  APP_PORT, IN_PROD, DB_USERNAME, DB_PASSWORD, DB_NAME
+} from '../config'
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0-ovg6n.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+
+    const app = express()
+
+    // app.use(isAuth)
+
+    app.disable('x-powered-by')
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      playground: !IN_PROD,
+      context: ({ req, res }) => ({ req, res })
+    })
+
+    server.applyMiddleware({ app })
+
+    app.listen({ port: APP_PORT }, () =>
+      console.log(`🚀 Server ready at http://localhost:${APP_PORT}${server.graphqlPath}`)
+    )
+  } catch (err) {
+    console.error(err)
+  }
+})()
