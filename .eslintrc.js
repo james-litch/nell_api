e89d29931fe5b2fc7153e46b7c52fd71e034ed19
James Litchfield <james.litchfield99@gmail.com>
2020-01-01 20:31:23 +0000

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-console":"off",
+    "no-unused-vars": "off",
+    "no-use-before-define": "off",
+    "func-names": "off",
+    "no-param-reassign": "off"
+  },
+};
