4c5a85a4d39882fb8d5e0a8c36ae2bb2f815c44e
James Litchfield <james.litchfield99@gmail.com>
2020-01-01 20:28:10 +0000

    added helper file

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,41 +1,28 @@
-import { AuthenticationError } from 'apollo-server-express'
-import jwt from 'jsonwebtoken'
-import { User } from '../models'
-import { JWT_KEY, JWT_TOKEN_LIFE } from '../../config'
+import { AuthenticationError } from 'apollo-server-express';
+import jwt from 'jsonwebtoken';
+import { User } from '../models';
+import { JWT_KEY, JWT_TOKEN_LIFE } from '../../config';
 
-const invalidDetails = 'Email or password is incorrect. Please try again.'
+const INVALID_CREDENTIALS = 'Email or password is incorrect. Please try again.';
+const AUTH_ERROR = 'You must be signed in.';
 
-export const checkSignedIn = user => {
-  console.log(user)
-  if (!user) throw new AuthenticationError('You must be signed in.')
-}
+const ensureSignedIn = (user) => {
+  if (!user) throw new AuthenticationError(AUTH_ERROR);
+};
 
-export const validateToken = token => {
-  let decodedToken
-
-  try {
-    decodedToken = jwt.verify(token, JWT_KEY)
-  } catch (err) {
-    decodedToken = null
-  }
-
-  return decodedToken
-}
-
-export const attemptSignIn = async (email, password) => {
+const attemptSignIn = async (email, password) => {
   // check email exists
-  const user = await User.findOne({ email: email })
-  if (!user) throw new AuthenticationError(invalidDetails)
+  const user = await User.findOne({ email });
+  if (!user) throw new AuthenticationError(INVALID_CREDENTIALS);
 
   // check password is correct
-  const match = await user.matchesPassword(password)
-  if (!match) throw new AuthenticationError(invalidDetails)
+  const match = await user.matchesPassword(password);
+  if (!match) throw new AuthenticationError(INVALID_CREDENTIALS);
 
-  return user
-}
+  return user;
+};
 
-export const generateToken = (id, email) => {
-  const token = jwt.sign({ id: id, email: email }, JWT_KEY, { expiresIn: `${JWT_TOKEN_LIFE}h` })
-
-  return token
-}
+export {
+  ensureSignedIn,
+  attemptSignIn,
+};
