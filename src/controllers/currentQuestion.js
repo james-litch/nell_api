aa2e0dcdf4bf146b8775c5784895f5d3d0ce20bc
James Litchfield <james.litchfield99@gmail.com>
2020-02-12 17:51:25 +0000

    added rough functionality

--- src/controllers/currentQuestion.js
+++ src/controllers/currentQuestion.js
@@ -1,1 +1,30 @@
-// TODO: add current question remove current question answer question.
+import { subjectExists, isCreator, validateInput } from './validate';
+import { SubjectValidation } from '../validators';
+
+const addCurrentQuestion = async ({
+  userId, subjectId, questionId, description,
+}) => {
+  validateInput({
+    userId, subjectId, questionId, description,
+  }, SubjectValidation.addCurrentQuestion);
+
+  const subject = await subjectExists(subjectId);
+
+  await isCreator(userId, subject);
+
+  const currentQuestion = await subject.updateOne({ $addToSet: { currentQuestions: {} } });
+};
+
+const removeCurrentQuestion = () => {
+
+};
+
+const answerCurrentQuestion = () => {
+
+};
+
+
+export {
+  addCurrentQuestion,
+  removeCurrentQuestion,
+};
