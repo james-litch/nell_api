f6cfc601f9606b12f9aaf78f70484d92012458fe
James Litchfield <james.litchfield99@gmail.com>
2020-02-12 17:54:29 +0000

    removed redundant strings, token functions

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,74 +1,63 @@
-import { AuthenticationError, UserInputError } from 'apollo-server-express';
 import { User } from '../models';
-import * as UserValidation from '../validators/user';
-import { generateToken, validateToken } from '../helpers/token';
+import { UserValidation } from '../validators';
+import { generateTokens } from '../helpers/token';
+import {
+  validateInput, isUser, matchesPassword, isUserEmail,
+} from './validate';
 
+const signUp = ({ name, email, password }) => {
+  validateInput({ name, email, password }, UserValidation.signUp);
 
-const INVALID_CREDENTIALS = 'Email or password is incorrect. Please try again.';
-const AUTH_ERROR = 'You must be signed in.';
-
-const signUp = (args) => {
-  // validate inputs
-  const { error } = UserValidation.signUp(args);
-  if (error) throw new UserInputError(error.details[0].message);
-
-  return User.create(args);
+  return User.create({ name, email, password });
 };
 
-const signIn = async (args) => {
-  // validate inputs
-  const { error } = UserValidation.signIn(args);
-  if (error) throw new UserInputError(error.details[0].message);
+const signIn = async ({ email, password }) => {
+  validateInput({ email, password }, UserValidation.signIn);
 
-  // check email exists
-  const user = await User.findOne({ email: args.email });
-  if (!user) throw new AuthenticationError(INVALID_CREDENTIALS);
+  const user = await isUserEmail(email);
 
-  // check password is correct
-  const match = await user.matchesPassword(args.password);
-  if (!match) throw new AuthenticationError(INVALID_CREDENTIALS);
-
+  await matchesPassword(user, password);
 
   // issue jwt token
-  return generateToken({
-    id: user.id,
-    email: user.email,
-    name: user.name,
-    subjects: user.subjects,
-  });
+  return generateTokens(
+    { id: user.id },
+    { id: user.id, count: user.tokenCount },
+  );
 };
 
-const ensureSignedIn = (user) => {
-  if (!user) throw new AuthenticationError(AUTH_ERROR);
-};
-
-const userFromToken = async (token) => {
-  // see if token is valid
-  const validToken = validateToken(token);
-  if (!validToken) return null;
-
-  return token;
+const invalidateToken = async (userId) => {
+  const user = isUser(userId);
+  user.updateOne({ $inc: { tokenCount: 1 } });
 };
 
 const getUser = async (id) => User.findById(id);
 
 const getUsers = async () => User.find({});
 
-const deleteAccount = () => {
-
+const deleteAccount = ({ userId }) => {
+// find all subjects and remove user from them
+// delete user
 };
 
-const changePassword = () => {
+const changePassword = async ({ userId, newPassword, oldPassword }) => {
+  console.log(oldPassword, newPassword);
+  validateInput({ userId, newPassword, oldPassword }, UserValidation.changePassword);
 
+  const user = await getUser(userId);
+
+  await matchesPassword(user, oldPassword);
+
+  await user.updateOne({ $set: { password: newPassword } });
+
+  return 'success';
 };
 
 export {
   signUp,
   signIn,
-  ensureSignedIn,
-  userFromToken,
   getUser,
   getUsers,
   deleteAccount,
   changePassword,
+  invalidateToken,
 };
