bc95b4500be92394f34b8490da470cf68d458640
James Litchfield <james.litchfield99@gmail.com>
2020-02-12 17:56:43 +0000

    now uses req instead of user

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -2,17 +2,34 @@ import { UserController, SubjectController } from '../../controllers';
 
 export default {
   Query: {
-    me: (root, args, { user }, info) => UserController.getUser(user.id),
+    me: (root, args, { req }, info) => UserController.getUser(req.userId),
 
-    users: (root, args, { user }, info) => UserController.getUsers(),
+    users: (root, args, { req }, info) => UserController.getUsers(),
 
-    user: (root, args, { user }, info) => UserController.getUser(args.id),
+    user: (root, args, { req }, info) => UserController.getUser(args.id),
   },
 
   Mutation: {
-    signUp: (root, { input }, context, info) => UserController.signUp(input),
+    signUp: (root, { input }, context, info) => UserController.signUp({
+      name: input.name,
+      email: input.email,
+      password: input.password,
+    }),
 
-    signIn: (root, { input }, context, info) => UserController.signIn(input),
+    signIn: (root, { input }, context, info) => UserController.signIn({
+      email: input.email,
+      password: input.password,
+    }),
+
+    invalidateToken: (root, { input }, { req }, info) => UserController.invalidateToken(req.userId),
+
+    changePassword: (root, { input }, { req }, info) => UserController.changePassword({
+      userId: req.userId,
+      newPassword: input.newPassword,
+      oldPassword: input.oldPassword,
+    }),
+    // TODO: finish
+    deleteAccount: (root, args, { req }, info) => UserController.deleteAccount(req.userId),
   },
 
   User: {
