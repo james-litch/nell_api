f8d32c4d6c6ef21b6403bb6042e50892c45636fa
James Litchfield <james.litchfield99@gmail.com>
2020-02-12 17:58:48 +0000

    added generate tokens method (refresh and access)

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -1,16 +1,23 @@
-import jwt from 'jsonwebtoken';
-import { JWT_KEY, JWT_TOKEN_LIFE } from '../../config';
+import { sign, verify } from 'jsonwebtoken';
+import { JWT_KEY, ACCESS_TOKEN_LIFE, REFRESH_TOKEN_LIFE } from '../../config';
 
 // token with args object as body.
 const generateToken = (args) => {
-  const token = jwt.sign(args, JWT_KEY, { expiresIn: `${JWT_TOKEN_LIFE}h` });
+  const token = sign(args, JWT_KEY, { expiresIn: `${ACCESS_TOKEN_LIFE}` });
   return { token };
+};
+
+const generateTokens = (accessBody, refreshBody) => {
+  const refreshToken = sign(refreshBody, JWT_KEY, { expiresIn: REFRESH_TOKEN_LIFE });
+  const accessToken = sign(accessBody, JWT_KEY, { expiresIn: ACCESS_TOKEN_LIFE });
+
+  return { accessToken, refreshToken };
 };
 
 const validateToken = (token) => {
   let validatedToken;
   try {
-    validatedToken = jwt.verify(token, JWT_KEY);
+    validatedToken = verify(token, JWT_KEY);
   } catch (err) {
     validatedToken = null;
   }
@@ -21,4 +28,5 @@ const validateToken = (token) => {
 export {
   generateToken,
   validateToken,
+  generateTokens,
 };
