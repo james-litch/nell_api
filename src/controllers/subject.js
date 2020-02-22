c2e9a7f0932da1b434d48975e7fa1a097dc2dc79
James Litchfield <james.litchfield99@gmail.com>
2020-02-22 15:44:00 +0000

    add create method

--- src/controllers/subject.js
+++ src/controllers/subject.js
@@ -1,5 +1,27 @@
-const create = () => {
+import { Subject, User } from '../models';
+import { validateInput } from './validate';
+import { SubjectInput } from '../validators';
 
+const create = async ({ userId, subjectName, password }) => {
+  // validate inputs.
+  validateInput({ subjectName, password }, SubjectInput.create);
+
+  // create subject.
+  const subject = await Subject.create({
+    name: subjectName,
+    admin: userId,
+    password,
+  });
+
+  // add subject to user.
+  const subjectObj = { subject: subject._id, admin: true };
+
+  await User.findOneAndUpdate(
+    { _id: userId },
+    { $addToSet: { subjects: subjectObj } },
+  );
+
+  return subject;
 };
 
 const join = () => {
