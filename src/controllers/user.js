a4d6701d43405598245166a93b6addaa5ad4359c
James Litchfield <james.litchfield99@gmail.com>
2020-02-22 14:38:39 +0000

    add sign up and sign in method

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,35 @@
-const signUp = () => {
+import { UserInput } from '../validators';
+import { User } from '../models';
+import { validateInput, matchesPassword, isUser } from './validate';
+import { generate, tokenBody } from './token';
 
+const signUp = async ({ name, email, password }) => {
+  // validate inputs.
+  validateInput({ name, email, password }, UserInput.signUp);
+
+  // create user.
+  const user = await User.create({ name, email, password });
+
+  // generate JWT refresh and access tokens.
+  const tokens = generate(tokenBody(user));
+
+  return { user, tokens };
 };
 
-const signIn = () => {
+const signIn = async ({ email, password }) => {
+  // validate inputs.
+  validateInput({ email, password }, UserInput.signIn);
 
+  // check email exists.
+  const user = await isUser(email);
+
+  // check password is correct.
+  await matchesPassword(user, password);
+
+  // generate JWT refresh and access tokens.
+  const tokens = generate(tokenBody(user));
+
+  return { user, tokens };
 };
 
 const endSession = () => {
@@ -14,8 +40,9 @@ const chnagePassword = () => {
 
 };
 
-const getUser = () => {
-
+const getUser = async ({ userId }) => {
+  const user = await User.find().where('_id').equals(userId).exec();
+  return user[0];
 };
 
 export {
