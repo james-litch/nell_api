b0cd2a1745bace55c7ad3de6a9684747b30496c6
James Litchfield <james.litchfield99@gmail.com>
2020-02-24 16:01:15 +0000

    add add question method

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,5 +1,29 @@
-const add = () => {
+import { validateInput } from './validate';
+import { QuestionInput } from '../validators';
+import { Question, Subject } from '../models';
 
+const add = async ({
+  userId, subjectId, question, answers, correctAnswer,
+}) => {
+  // validate inputs.
+  validateInput({
+    subjectId, question, answers, correctAnswer,
+  }, QuestionInput.add);
+
+  const answersObj = [];
+  answers.forEach((answer) => { answersObj.push({ answer, totalChosen: 0 }); });
+
+  // add question to questions.
+  const createdQuestion = await Question.create({
+    question, answers: answersObj, correctAnswer,
+  });
+
+  // add question to subjects
+  const update = await Subject.findOneAndUpdate(
+    { _id: subjectId },
+    { $addToSet: { questions: createdQuestion._id } },
+  );
+  return createdQuestion;
 };
 
 const remove = () => {
