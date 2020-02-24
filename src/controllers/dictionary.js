80d10c10b3c4b99404686afc451c1869e0bc0eaa
James Litchfield <james.litchfield99@gmail.com>
2020-02-24 13:29:23 +0000

    add / remove defs

--- src/controllers/dictionary.js
+++ src/controllers/dictionary.js
@@ -1,9 +1,34 @@
-const add = () => {
+import { validateInput } from './validate';
+import { DictionaryInput } from '../validators';
+import { Subject } from '../models';
 
+const add = async ({
+  userId, subjectId, phrase, definition,
+}) => {
+  // validate inputs
+  validateInput({ subjectId, phrase, definition }, DictionaryInput.add);
+
+  // add definition to dictionary.
+  const update = await Subject.findOneAndUpdate(
+    { _id: subjectId },
+    { $addToSet: { dictionary: { phrase, definition } } },
+    { new: true },
+  );
+  console.log(update);
+  return { phrase, definition };
 };
 
-const remove = () => {
+const remove = async ({ userId, subjectId, definitionIds }) => {
+  // validate inputs
+  validateInput({ subjectId, definitionIds }, DictionaryInput.remove);
 
+  // remove definition from dictionary.
+  const update = await Subject.findOneAndUpdate(
+    { _id: subjectId },
+    { $pull: { dictionary: { _id: { $in: definitionIds } } } },
+  );
+
+  return 'success';
 };
 
 const find = () => {
