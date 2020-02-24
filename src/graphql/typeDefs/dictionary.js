2fb4e88bd9c4105ad08934a654a79716648906b5
James Litchfield <james.litchfield99@gmail.com>
2020-02-24 13:31:09 +0000

    add admin directive to mutations

--- src/graphql/typeDefs/dictionary.js
+++ src/graphql/typeDefs/dictionary.js
@@ -3,8 +3,8 @@ import { gql } from 'apollo-server-express';
 export default gql`
 
   extend type Mutation{
-     addDefinition(input: AddDefinition): Definition @auth
-     removeDefinitions(input: RemoveDefinitions): String @auth
+     addDefinition(input: AddDefinition): Definition @admin
+     removeDefinitions(input: RemoveDefinitions): String @admin
   }
 
   type Definition{
