2914b4a0138691fbcd5e33bec447152416364d96
James Litchfield <james.litchfield99@gmail.com>
2020-02-24 16:03:23 +0000

    add exam input validator

---
+++ src/validators/exam.js
@@ -0,0 +1,22 @@
+import Joi from '@hapi/joi';
+import {
+  id, idArray, name, description,
+} from './fields';
+
+const create = (data) => {
+  const schema = Joi.object({
+    subjectId: id, name, description, questions: idArray,
+  });
+  return schema.validate(data);
+};
+
+const remove = (data) => {
+  const schema = Joi.object({
+    subjectId: id, examIds: idArray,
+  });
+  return schema.validate(data);
+};
+
+export {
+  create, remove,
+};
